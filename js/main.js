1ba2c1c5347ecdd0a52a9b0cf160eb0d43a5e586
MKW420 <keitumetseshongwe@gmail.com>
2022-05-05 14:36:51 +0200

    navigation bar fixed

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.querySelector('.moon').onclick = () =>{
+    document.querySelector('body').classList.toggle('is-black');
+
+}
+
+document.querySelector('.menu-btn').onclick = () =>{
+    document.querySelector('.header-menu').classList.toggle('is-active');
+}
